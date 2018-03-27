5b4a06898d049934b51b97e000c713cfbae72d01
Raphael Villas <raphael.villas@gmail.com>
2018-03-26 23:16:45 -0500

    Fix layout and add string var

--- app/src/main/java/com/studio2bigdiv/android/justjava/MainActivity.java
+++ app/src/main/java/com/studio2bigdiv/android/justjava/MainActivity.java
@@ -31,7 +31,8 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        displayPrice(quantity * 5);
+        String priceMessage = "Free";
+        displayMessage(priceMessage);
     }
 
     /**
@@ -40,6 +41,14 @@ public class MainActivity extends AppCompatActivity {
     private void display(int number) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + number);
+    }
+
+    /**
+     * This method displays the given text on the screen.
+     */
+    private void displayMessage(String message) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(message);
     }
 
     /**
